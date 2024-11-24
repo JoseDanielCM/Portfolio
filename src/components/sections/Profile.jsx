363514db0684021d0abf24de8119f6704fb12bd4
JoseDanielCM <jodacarmon@gmail.com>
2024-11-24 07:21:00 -0500

    feat: :sparkles: Responsive profile

--- src/components/sections/Profile.jsx
+++ src/components/sections/Profile.jsx
@@ -4,17 +4,13 @@ import ProfileCard from '../InternalSections/ProfileCard';
 
 function Profile() {
     return (
-        <section id='Profile' className="main-section sectBack-Bot relative h-screen flex items-center justify-center overflow-hidden m-0">
+        <section id='Profile' className="main-section sectBack-Bot relative h-fit md:h-screen flex items-center justify-center overflow-hidden m-0">
             {/* Background Video */}
             <video
                 autoPlay
                 muted
                 loop
-                className="absolute inset-0 w-full h-full object-cover z-0"
-                style={{
-                    transform: 'translateX(-25vw) scale(1.5)',
-                    objectFit: 'cover'
-                }}
+                className="videoProfile absolute inset-0 w-full h-full object-cover z-0"
             >
                 <source src="/ProfileBackground.mp4" type="video/mp4" />
             </video>
@@ -22,9 +18,9 @@ function Profile() {
             {/* Dark overlay */}
 
             {/* Content */}
-            <div className="w-screen relative z-20 flex flex-col md:flex-row items-center justify-between text-white right-24 p-0">
+            <div className="w-screen relative z-20 flex flex-col md:flex-row md:items-center justify-center text-white p-0">
                 {/* Profile Photo */}
-                <div className="w-44 h-44 md:w-1/6 md:h-1/6  bg-gray-800 rounded-full overflow-hidden flex-shrink-0 mb-4 md:mb-0 relative" style={{ marginLeft: '23.5%', marginBottom:"1%" }}>
+                <div className="profilePhoto w-44 h-44 md:w-1/6 md:h-1/6  bg-gray-800 rounded-full overflow-hidden flex-shrink-0 mb-4 md:mb-0 relative">
                     <img src="/public/ProfilePhoto.png" alt="Profile Photo" className="w-full h-full object-cover" />
                 </div>
                 <ProfileCard/>
