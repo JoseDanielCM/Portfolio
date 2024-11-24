3b54c2e63459b6113419c9fa9e513e9f97eb096a
JoseDanielCM <jodacarmon@gmail.com>
2024-11-23 23:21:11 -0500

    feat: :sparkles: Add profile background and image

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,29 @@
+// MainContent.jsx
+import React, { useEffect } from 'react';
+import Profile from './sections/Profile'
+// import Goals from './sections/Goals';
+// import Projects from './sections/Projects'
+// import Mission from './sections/Mission'
+import { initScrollObserver } from '../scrollObserver';
+
+function MainContent() {
+    useEffect(() => {
+        // Llamar la función para inicializar el observer
+        const cleanupObserver = initScrollObserver();
+
+        // Limpiar el observer al desmontar el componente
+        return cleanupObserver;
+    }, []);
+
+    return (
+        <main className='p-0 bg-black'>
+            <Profile />
+            {/* 
+            <Goals/>
+            <Mission/>
+            <Projects/>*/}
+        </main>
+    );
+}
+
+export default MainContent;
